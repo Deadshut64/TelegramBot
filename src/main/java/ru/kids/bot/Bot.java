52052f9389bbb2006c5843d8ffe8c6d9889a7283
Deadshut64 <sergeychuchmanov@gmail.com>
2022-09-29 16:46:10 +0400

    hello

--- src/main/java/ru/kids/bot/Bot.java
+++ src/main/java/ru/kids/bot/Bot.java
@@ -1,24 +1,19 @@
 package ru.kids.bot;
 
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.kids.bot.command.ShowUsersCommand;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.kids.configuration.BotProperties;
 
-import java.util.List;
-
 @Component
-public class Bot extends TelegramLongPollingCommandBot {
+public class Bot extends TelegramLongPollingBot {
 
     private final BotProperties properties;
 
     public Bot(BotProperties properties) {
         this.properties = properties;
-        register(new ShowUsersCommand());
-        register(new HelpCommand("help", "Помощь", ""));
     }
 
     @Override
@@ -32,27 +27,47 @@ public class Bot extends TelegramLongPollingCommandBot {
     }
 
     @Override
-    public void onRegister() {
-        super.onRegister();
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            long chatId = update.getMessage().getChatId();
+
+            switch (message){
+                case "/start":
+                    try {
+                        startCommand(chatId,update.getMessage().getChat().getFirstName());
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                default:
+                    try {
+                        sendMessage(chatId,"пу пу пу");
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+
+            }
+
+
+
+        }
     }
-
-    @Override
-    public void processNonCommandUpdate(Update update) {
-
+    private void startCommand(long chatId, String name) throws TelegramApiException {
+        String hello = "Привет " + name + "выбирай пол ребенка Пидр";
+        sendMessage(chatId, hello);
     }
+    private void sendMessage(long chatId, String text) throws TelegramApiException {
+        SendMessage sm = new SendMessage();
+        sm.setChatId(String.valueOf(chatId));
+        sm.setText(text);
 
-    @Override
-    public void processInvalidCommandUpdate(Update update) {
-        super.processInvalidCommandUpdate(update);
-    }
+        try{
+            execute(sm);
+        } catch (TelegramApiException e){
 
-    @Override
-    public boolean filter(Message message) {
-        return super.filter(message);
-    }
+        }
 
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-        super.onUpdatesReceived(updates);
     }
 }
+
+
