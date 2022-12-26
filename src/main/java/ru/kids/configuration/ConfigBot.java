aaec18611723b93f781eb49f930601754fc13e3b
Deadshut64 <sergeychuchmanov@gmail.com>
2022-12-26 16:35:50 +0400

    testBot2

--- src/main/java/ru/kids/configuration/ConfigBot.java
+++ src/main/java/ru/kids/configuration/ConfigBot.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -13,22 +12,22 @@ import ru.kids.bot.Bot;
 
 import javax.annotation.PostConstruct;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 @ConditionalOnProperty(value = "bot-configuration.enabled-production", havingValue = "true", matchIfMissing = true)
 public class ConfigBot {
 
-    @Autowired
-    public Bot Bot(BotProperties botProperties){
+    @Bean
+    public Bot bot(BotProperties botProperties){
         return new Bot(botProperties);
     }
 
     @PostConstruct
-    public void init(Bot bot) {
+    public void init() {
         TelegramBotsApi botsApi;
         try {
             botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(bot);
+            botsApi.registerBot();
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
