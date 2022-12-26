b1dfe52c1bb1de595bc38431e226db9dba7323e7
Alexey Storozhev <a.storozhev@crpt.ru>
2022-12-26 15:36:23 +0300

    fix: start bot

--- src/main/java/ru/kids/configuration/ConfigBot.java
+++ src/main/java/ru/kids/configuration/ConfigBot.java
@@ -2,7 +2,6 @@ package ru.kids.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
